2911de3f080fe13a573489a98ad5ce909b8d96fc
srinivas <srinivas23peddada@gmail.com>
2025-11-11 12:02:21 +0530

    PUT and DELETE operations are added in NODEMONGO file

--- NodeMongo/controllers/employeeController.js
+++ NodeMongo/controllers/employeeController.js
@@ -19,4 +19,64 @@ const createEmployee = async(req,res)=>{
 }
 
 
-module.exports = {createEmployee}
+const getEmployees = async(req, res)=>{
+    try{
+        const employees = await Employee.find()
+        res.status(200).json(employees)
+    }
+    catch(err){
+        console.log(`There is an error`, err)
+        res.status(500). json({message:"Server Error"})
+    }
+}
+
+
+const singleEmployee = async(req, res)=>{
+    try{
+        const empID = await Employee.findById(req.params.id)
+        if (!empID){
+            return res.status(404).json({message:"Employee not found"})
+        }
+        res.status(200).json(empID)
+    }
+    catch(err){
+        console.log(`There is an error`, err)
+        res.status(500).json({message:"Server Error"})
+    }
+
+}
+
+
+
+const updateEmployee = async(req, res)=>{
+    try{
+        const {name, email, phone, city} = req.body
+        const myEmployee = await Employee.findByIdAndUpdate(
+            req.params.id,
+            {name, email, phone, city}
+        )
+        if(!myEmployee){
+            return res.status(404).json({message:"Employee not found"})
+        }
+        res.status(200).json(myEmployee)
+    }
+    catch(err){
+        console.log(`There is an error`, err)
+        res.status(500).json({message:"Server Error"})
+    }
+}
+
+
+const deleteEmployee = async(req, res)=>{
+    try{
+        const deleteEmployee = await Employee.findByIdAndDelete(req.params.id)
+        res.status(200).send()
+    }
+    catch(err){
+        console.log(`There is an error`, err)
+        res.status(500).json({message:`Employee Not Found`})
+    }
+}
+
+
+module.exports = {createEmployee, getEmployees, singleEmployee, updateEmployee, deleteEmployee}
--- NodeMongo/routes/employeRoutes.js
+++ NodeMongo/routes/employeRoutes.js
@@ -6,6 +6,9 @@ const Employee = require('../models/Employee')
 
 
 router.post('/add-emp', employeeController.createEmployee)
-
+router.get('/allEmployees', employeeController.getEmployees)
+router.get('/employee/:id', employeeController.singleEmployee)
+router.put('/update/:id', employeeController.updateEmployee)
+router.delete('/delete/:id', employeeController.deleteEmployee)
 
 module.exports = router
